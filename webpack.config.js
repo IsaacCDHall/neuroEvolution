f3d06b564a358ed6525a6edfc1a9f2c87e768462
Isaac Hall <isaachalldev@gmail.com>
2020-01-26 18:22:45 -0800

    successful webpack html loader configuration for production deploy, moved bundle to dist build

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,50 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const ROOT = path.resolve( __dirname, 'src' );
 const DESTINATION = path.resolve( __dirname, 'dist' );
 
 module.exports = {
-    context: ROOT,
+  context: ROOT,
 
-    entry: {
-        'main': './main.js'
-    },
+  entry: {
+    main: "./main.js"
+  },
 
-    output: {
-        filename: '[name].bundle.js',
-        path: DESTINATION
-    },
+  output: {
+    filename: "[name].bundle.js",
+    path: DESTINATION
+  },
 
-    resolve: {
-        extensions: ['.jsx', '.js'],
-        modules: [
-            ROOT,
-            'node_modules'
-		]
-	},
+  resolve: {
+    extensions: [".jsx", ".js"],
+    modules: [ROOT, "node_modules"]
+  },
 
-    module: {
-        rules: [
-            /****************
-            * PRE-LOADERS
-            *****************/
-            {
-                enforce: 'pre',
-                test: /\.js$/,
-                use: 'source-map-loader'
-            }
-        ]
-    },
+  module: {
+    rules: [
+      /****************
+       * PRE-LOADERS
+       *****************/
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        use: "source-map-loader"
+      }
+    ]
+  },
 
-    devtool: 'cheap-module-source-map',
-    devServer: {}
+  devtool: "cheap-module-source-map",
+  devServer: {},
+  plugins: [
+    // New plugin
+    new HtmlWebpackPlugin({
+      // injects bundle.js to our new index.html
+      inject: true,
+      // copys the content of the existing index.html to the new /build index.html
+      path: DESTINATION,
+      template: path.resolve("./src/index.html")
+    })
+  ]
 };
